8b7450c9e114e1abd12b729a95e42ed23e7edf6e
ericka.bermudez <37844174+ErickaBermudez@users.noreply.github.com>
2023-02-03 16:58:45 +0900

    [PK-03] Change layout color

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,17 @@
-import React from 'react'; 
+// imports
+import React from 'react';
+import { pokemonColors } from '../common/pokemonColors';
+// components
 const pokeball = require('../assets/images/pokeball.png');
+// variables
+const backgroundColor = 'rgb(248 250 252)';
+var selectedPokemon: string | null;
 
-export const MainLayout = () => <div className='w-full h-screen bg-slate-50 p-10'>
+export const MainLayout = () => <div className={`w-full h-screen p-10`} style={{backgroundColor: selectedPokemon? selectedPokemon : backgroundColor}}>
     <div className='flex items-center'>
-    <img src={pokeball} alt="Pokeball" className='w-10 h-10 mr-3'/>
-  <h1 className='font-kulim-park text-2xl'>Pokedex</h1>
+        <img src={pokeball} alt="Pokeball" className='w-10 h-10 mr-3' />
+        <h1 className='font-kulim-park text-2xl'>Pokedex</h1>
     </div>
-  <p>
-  </p>
+    <p>
+    </p>
 </div>
