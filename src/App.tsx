ad4fb7a37f944fade90d3c3fdd9e5e8bbbc42bbb
ericka.bermudez <37844174+ErickaBermudez@users.noreply.github.com>
2023-02-03 15:27:43 +0900

    [PK-01] JS->TS and base structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-red-400">
+        Pokedex!
+      </header>
+    </div>
+  );
+}
+
+export default App;
