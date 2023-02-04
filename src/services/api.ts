f23f88384f92d95151e6985eef8fe5b61c62d0f8
ericka.bermudez <37844174+ErickaBermudez@users.noreply.github.com>
2023-02-04 18:13:10 +0900

    [PK-Testing] Don't allow id look up

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,6 +9,13 @@ const baseURL = 'https://pokeapi.co/api/v2';
 export const getPokemon = async (pokemonName: string) => {
     let notFoundMessage = "Oops, are you sure that Pokemon exists?"
     try {
+        /**
+         * If value is a number, our API point can still look for the 
+         * pokemon with that ID. For now, just focus on Pokemon name. 
+         */
+        const intValue = parseInt(pokemonName, 10);
+        if(!isNaN(intValue) && typeof intValue === "number") return Promise.reject(notFoundMessage);
+        
         let url = `${baseURL}/pokemon/${pokemonName}`;
         const response = await axios.get(url)
         if(response?.data){
